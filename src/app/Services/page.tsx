c315b10befefa20edb2b5e9702c8a51e89a2051a
david-mulwa <mulwakitonga2000@gmail.com>
2024-07-03 17:18:22 +0300

    services

--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Section from "../../components/Section";
-import {services} from '../../components/constants'
+import {services, prog} from '../../components/constants'
 import Image from 'next/image'
 const Services = () => {
   return (
@@ -24,6 +24,27 @@ const Services = () => {
           </div>
         ))}
       </div>
+      <div className=' flex lg:flex-nowrap xl:flex-nowrap md:flex-wrap sm:flex-wrap gap-5  '>
+      
+      <div className='w-full gap-5 space-y-5 xl:w-[69%] lg:w-[70%]'>
+        {prog.map((item)=>(
+          <div key={item.id} className= 'flex flex-col gap-1 group group:ease-in-out '>
+            <label className='flex items-center justify-between'>
+              <span className='font-bold text-xl '>{item.name}</span>
+              <span className='font-bold text-xl '>{item.value}</span>
+            </label>
+            <progress color="blue" value={item.value} max='100' className='bg-gray-500 w-full transition ease-in-out delay-150 color-blue-200'/>
+          </div>
+        ))}
+      </div>
+
+
+      <div className='flex flex-col  w-full xl:w-[31%] lg:w-[30%] h-auto border-l-4 p-3 border-black md:mt-5 sm:mt-7'>
+        <h1 className='text-3xl font-bold '>Our Year In Numbers</h1>
+        <p className='text-balance mt-12 text-lg'>We revolutionize the way sales teams interact with customers. Our success has been driven by your passion for great selling</p>
+        
+      </div>    
+    </div>
     </Section>
   )
 }
