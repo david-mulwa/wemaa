633a2f894ca13f62427e48d3efefa450d6648ceb
david-mulwa <mulwakitonga2000@gmail.com>
2024-07-05 10:02:42 +0300

    add className

--- src/components/sertag.tsx
+++ src/components/sertag.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const sertag = () => {
+const sertag = ({className}:{className:string}) => {
   return (
-    <div className={`flex flex-col  w-full h-auto border-l-4 p-3 border-black `}>
+    <div className={`flex flex-col  w-full h-auto border-l-4 p-3 border-black ${className} `}>
         <h1 className='text-4xl font-bold mb-3'>Our Services</h1>
         <p className='text-balance text-gray-800 text-lg'>Our company provides services in courier clearing and forwarding as well as parcel carrier job. We understand the importance of delivering packages on time and ensuring they reach their intended destination safely. With our experienced team and reliable network of transportation partners, we offer professional handling and clearance for all types of shipments.</p>
     </div>
