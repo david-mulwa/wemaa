191e94b3b3d082cfdb661ac98c52a00239a87940
david-mulwa <mulwakitonga2000@gmail.com>
2024-07-06 14:52:38 +0300

    login

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -10,7 +10,7 @@ import {
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
 
-const SignIn=()=> {
+const Login=()=> {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -26,16 +26,7 @@ const SignIn=()=> {
         </p>
 
         <form className="my-8" onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label  htmlFor="firstname">First name</Label>
-              <Input id="firstname" placeholder="Tyler" type="text" />
-            </LabelInputContainer>
-            <LabelInputContainer>
-              <Label htmlFor="lastname">Last name</Label>
-              <Input id="lastname" placeholder="Durden" type="text" />
-            </LabelInputContainer>
-          </div>
+          
           <LabelInputContainer className="mb-4">
             <Label htmlFor="email">Email Address</Label>
             <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
@@ -44,14 +35,7 @@ const SignIn=()=> {
             <Label htmlFor="password">Password</Label>
             <Input id="password" placeholder="••••••••" type="password" />
           </LabelInputContainer>
-          <LabelInputContainer className="mb-8">
-            <Label htmlFor="twitterpassword">Your twitter password</Label>
-            <Input
-              id="twitterpassword"
-              placeholder="••••••••"
-              type="twitterpassword"
-            />
-          </LabelInputContainer>
+          
 
           <button
             className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
@@ -60,7 +44,7 @@ const SignIn=()=> {
             Sign up &rarr;
             <BottomGradient />
           </button>
-          <span className='text-white mt-8 text-lg '>Have an account     <a href='/Login' className='text-gray-200 hover:text-whitesmoke'> LogIn &rarr;</a></span>
+          <span className='text-white mt-8 text-lg '>Don&apos;t Have an account     <a href='/Signin' className='text-gray-200 hover:text-whitesmoke'> Sign Up &rarr;</a></span>
 
           <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
@@ -92,7 +76,7 @@ const SignIn=()=> {
     </Section>
   );
 }
-export default SignIn
+export default Login
 
 const BottomGradient = () => {
   return (
--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -58,7 +58,7 @@ export const FloatingNav = ({
             <span className="hidden sm:block text-lg font-bold uppercase">{navItem.name}</span>
           </Link>
         ))}
-        <Button href='/SignIn' className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+        <Button href='/SignIn' className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black hover:text-white dark:text-white px-4 py-2 rounded-full">
           <span>Login</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
         </Button>
